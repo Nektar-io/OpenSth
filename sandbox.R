c1b22dd58201169c2fffb444d3aed940b5b2bedc
Love Hansson <love.hansson@gmail.com>
2014-01-05 12:26:51 +0100

    Sandbox cleanup

--- sandbox.R
+++ sandbox.R
@@ -1,12 +1,5 @@
-e <- l[[1]]
-df <- data.frame(l[[1]], stringsAsFactors=FALSE)
-df2 <- df
-
-for(i in 2:length(l)) {
-	df_temp <- data.frame(l[[i]], stringsAsFactors=FALSE)
-	df2 <- rbind(df2, df_temp)
-}
-
+require(projectX)
+require(LvWS)
 
 ## Skapa ett adressobjekt
 jag <- sthAddr("Erkenskroken", 24)
