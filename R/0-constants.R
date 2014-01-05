3945e9e49bbf12b1fb887a3682416dfddc186364
Love Hansson <love.hansson@gmail.com>
2014-01-05 17:15:33 +0100

    Move SL key to projectX

--- R/0-constants.R
+++ R/0-constants.R
@@ -2,14 +2,13 @@
 .placesKey <- "19d54f2ed3f842569c94f4375175957e"
 .unitKey <- "0eb1055a722f4b65986f545cb67bd44e"
 .lvwsKey <- "74c448e2-e118-4ed7-aceb-e0a2864ed1a2"
-.slKey <- "dk5ZlsqLXiEdyHYVfTR2y56GR63a6Ejs"
 
 # URLs and paths
 .unitUrl <- "http://api.stockholm.se"
 .placesUrl <- "http://api.stockholm.se"
 
 .lvwsUrl <- "http://openstreetws.stockholm.se/"
-.lwvsPath <- "LvWS-2.2/Lv.asmx"
+.lvwsPath <- "LvWS-2.2/Lv.asmx"
 
 
 
